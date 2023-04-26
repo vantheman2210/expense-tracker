a91104e7bdae91ee1f2dce3a16fa256722f66a6c
vantheman2210 <martinovic.ba@gmail.com>
2023-04-26 14:44:40 +0200

    Implemented .map to dynamically add expenses

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem';
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
-      <ExpenseItem />
-    </div>
-  );
+	const expenses = [
+		{
+			id: 'e1',
+			title: 'Toilet Paper',
+			amount: 94.12,
+			date: new Date(2023, 7, 14)
+		},
+		{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 2, 12) },
+		{
+			id: 'e3',
+			title: 'Car Insurance',
+			amount: 294.67,
+			date: new Date(2023, 2, 28)
+		},
+		{
+			id: 'e4',
+			title: 'New Desk (Wooden)',
+			amount: 450,
+			date: new Date(2023, 5, 12)
+		}
+	];
+	return (
+		<div>
+			{expenses.map((expense) => (
+				<div key={expense.id}>
+					<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = function() {
+const ExpenseItem = function(props) {
 	const expenseDate = new Date(2023, 2, 28);
 	const expenseTitle = 'Car Insurance';
 	const expenseAmount = 259.99;
 	return (
 		<div className="expense-item">
-			<div>{expenseDate.toISOString()}</div>
+			<div>{props.date.toISOString()}</div>
 			<div className="expense-item__description">
-				<h2>{expenseTitle}</h2>
-				<div className="expense-item__price">${expenseAmount}</div>
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
 			</div>
 		</div>
 	);
