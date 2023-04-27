51657ff71d34659578ad9396becac677d12ca175
vantheman2210 <martinovic.ba@gmail.com>
2023-04-27 09:53:07 +0200

    Added card component to Expenses.js

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,16 +1,17 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 const Expenses = function (props) { 
   const expenses = props.expenses;
   return (
-    <div className="expenses">
+    <Card className="expenses">
 			{expenses.map((expense) => (
 				<div key={expense.id}>
 					<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
 				</div>
 			))}
-		</div>
+		</Card>
   )
 };
 
