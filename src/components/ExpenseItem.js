628a3d22e14843a9bdfc96ff907c0d19b36c273d
vantheman2210 <martinovic.ba@gmail.com>
2023-04-27 09:21:15 +0200

    Created date component, added css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = function(props) {
-	// Constant variables 
-	
-	
-	
+	// Constant variables
+
 	return (
 		<div className="expense-item">
-			<div>
-			<div>{month}</div>
-			<div>{year}</div>
-			<div>{day}</div>
-			</div>
+			<ExpenseDate date={props.date}/>
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
