7a835b0009c82f73b30ad832fe475fca144ef4c0
vantheman2210 <martinovic.ba@gmail.com>
2023-04-27 09:08:22 +0200

    Removed hardcode variables, used props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = function(props) {
-	const expenseDate = new Date(2023, 2, 28);
-	const expenseTitle = 'Car Insurance';
-	const expenseAmount = 259.99;
+	// Constant variables 
+	const month = props.date.toLocaleString('en-US', {month: 'long'});
+	const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+	const year = props.date.getFullYear();
+	
+	
 	return (
 		<div className="expense-item">
-			<div>{props.date.toISOString()}</div>
+			<div>
+			<div>{month}</div>
+			<div>{year}</div>
+			<div>{day}</div>
+			</div>
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
