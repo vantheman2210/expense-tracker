2f7673241fc2d3528614fdb68b1794f57f12ae9a
vantheman2210 <martinovic.ba@gmail.com>
2023-04-27 09:52:25 +0200

    Adding props children and css styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import './Card.css'
 
-const Card = function () { 
+const Card = function (props) { 
+  const classes = 'card ' + props.className
   return (
-    <div className="card"></div>
+    <div className={classes}>{props.children}</div>
   )
 };
-
+// You do not set children prop. It is a reserved name. It is always content between opening and closing tag 
+// Have to add classes if you want css to apply
 export default Card;
