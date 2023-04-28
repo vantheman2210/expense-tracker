fa84076296a0190e32b1e5f1856316c39873388d
vantheman2210 <martinovic.ba@gmail.com>
2023-04-28 11:14:09 +0200

    Changed year values in expensesfilter.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,29 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
-const Expenses = function (props) { 
-  const expenses = props.expenses;
-  return (
-    <Card className="expenses">
-			{expenses.map((expense) => (
-				<div key={expense.id}>
-					<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
-				</div>
-			))}
-		</Card>
-  )
+const Expenses = function(props) {
+	const expenses = props.expenses;
+	const [filterYear, setFilterYear] = useState('2021');
+
+	const filterYearSelected = (year) => {
+		console.log(year);
+		setFilterYear(year);
+	};
+	return (
+		<div>
+			<ExpensesFilter selected={filterYear} getFilterYear={filterYearSelected} />
+			<Card className="expenses">
+				{expenses.map((expense) => (
+					<div key={expense.id}>
+						<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+					</div>
+				))}
+			</Card>
+		</div>
+	);
 };
 
 export default Expenses;
