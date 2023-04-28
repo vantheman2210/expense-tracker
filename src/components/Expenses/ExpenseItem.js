9867f45d79e4393f609ea88d6688d34bb980d9fa
vantheman2210 <martinovic.ba@gmail.com>
2023-04-28 13:59:30 +0200

    Wrapped jsx in li element in expensesitem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,22 +4,16 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = function(props) {
-	// State 
-	const [title, setTitle] = useState(props.title);
-
-	const clickHandler = () => { 
-		console.log('Click')
-		setTitle('Updated')
-	}
-
 	return (
-		<Card className="expense-item">
-			<ExpenseDate date={props.date}/>
-			<div className="expense-item__description">
-				<h2>{title}</h2>
-				<div className="expense-item__price">${props.amount}</div>
-			</div>
-		</Card>
+		<li>
+			<Card className="expense-item">
+				<ExpenseDate date={props.date} />
+				<div className="expense-item__description">
+					<h2>{props.title}</h2>
+					<div className="expense-item__price">${props.amount}</div>
+				</div>
+			</Card>
+		</li>
 	);
 };
 
